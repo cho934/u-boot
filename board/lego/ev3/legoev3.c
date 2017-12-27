42160df71c26a4344541e184c3a5b8b470f6f45d
David Lechner <david@lechnology.com>
2017-12-27 17:48:02 -0600

    ARM: legoev3: disable networking
    This disables networking related items in the config. The EV3 does not have
    any networking hardware, so this is wasted space.
    
    Signed-off-by: David Lechner <david@lechnology.com>

--- board/lego/ev3/legoev3.c
+++ board/lego/ev3/legoev3.c
@@ -15,8 +15,6 @@
 
 #include <common.h>
 #include <i2c.h>
-#include <net.h>
-#include <netdev.h>
 #include <spi.h>
 #include <spi_flash.h>
 #include <asm/arch/hardware.h>
@@ -163,7 +161,7 @@ int board_init(void)
 
 	/* setup the SUSPSRC for ARM to control emulation suspend */
 	writel(readl(&davinci_syscfg_regs->suspsrc) &
-	       ~(DAVINCI_SYSCFG_SUSPSRC_EMAC | DAVINCI_SYSCFG_SUSPSRC_I2C |
+	       ~(DAVINCI_SYSCFG_SUSPSRC_I2C |
 		 DAVINCI_SYSCFG_SUSPSRC_SPI0 | DAVINCI_SYSCFG_SUSPSRC_TIMER0 |
 		 DAVINCI_SYSCFG_SUSPSRC_UART1),
 	       &davinci_syscfg_regs->suspsrc);
